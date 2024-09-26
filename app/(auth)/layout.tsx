e9e23822a26ae225aa1484a50c5f48637c588e53
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-26 19:03:05 +0530

    Modify the auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="flex justify-between w-full min-h-screen font-inter">
       {children}
+      <div className="auth-asset">
+        <div>
+          <Image
+            src={"/icons/auth-image.svg"}
+            alt="Auth Image"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </main>
   );
 }
