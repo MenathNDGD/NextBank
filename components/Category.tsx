138ee87b5f5ecda79d1e9dc0fe30662e1fff1646
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-28 15:04:14 +0530

    Create the category component

---
+++ components/Category.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+import { topCategoryStyles } from "@/constants";
+import { cn } from "@/lib/utils";
+
+import { Progress } from "./ui/progress";
+
+const Category = ({ category }: CategoryProps) => {
+  const {
+    bg,
+    circleBg,
+    text: { main, count },
+    progress: { bg: progressBg, indicator },
+    icon,
+  } = topCategoryStyles[category.name as keyof typeof topCategoryStyles] ||
+  topCategoryStyles.default;
+
+  return (
+    <div className={cn("gap-[18px] flex p-4 rounded-xl", bg)}>
+      <figure className={cn("flex-center size-10 rounded-full", circleBg)}>
+        <Image src={icon} width={20} height={20} alt={category.name} />
+      </figure>
+      <div className="flex flex-col flex-1 w-full gap-2">
+        <div className="flex justify-between text-14">
+          <h2 className={cn("font-medium", main)}>{category.name}</h2>
+          <h3 className={cn("font-normal", count)}>{category.count}</h3>
+        </div>
+        <Progress
+          value={(category.count / category.totalCount) * 100}
+          className={cn("h-2 w-full", progressBg)}
+          indicatorClassName={cn("h-2 w-full", indicator)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Category;
