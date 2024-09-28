285aea1eabea82618a60e739147fe35ff05f13bb
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-28 14:57:16 +0530

    Modify the payment transfer page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,31 @@
+import HeaderBox from "@/components/HeaderBox";
+import PaymentTransferForm from "@/components/PaymentTransferForm";
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const PaymentTransfer = () => {
-  return <div>PaymentTransfer</div>;
+const PaymentTransfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+
+  return (
+    <section className="payment-transfer">
+      <HeaderBox
+        title="Payment Transfer"
+        subtext="Transfer money to your friends and family"
+      />
+
+      <section className="pt-5 size-full">
+        <PaymentTransferForm accounts={accountsData} />
+      </section>
+    </section>
+  );
 };
 
 export default PaymentTransfer;
