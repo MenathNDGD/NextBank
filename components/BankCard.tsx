0a90ae7904134b332bc0537393ba12394c4f72d5
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-28 14:58:08 +0530

    Modify the bank card component

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { formatAmount } from "@/lib/utils";
+import CopyCardId from "./CopyCardId";
 
 const BankCard = ({
   account,
@@ -10,10 +11,13 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href={"/"} className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
-            <h1 className="font-semibold text-white text-16">{userName}</h1>
+            <h1 className="font-semibold text-white text-16">{account.name}</h1>
             <p className="font-black text-white font-ibm-plex-serif">
               {formatAmount(account.currentBalance)}
             </p>
@@ -49,6 +53,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <CopyCardId title={account?.shareableId} />}
     </div>
   );
 };
